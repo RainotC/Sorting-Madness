f23d41cd772d08734951254bf6aff1a7f7532ae9
Mikołaj Kiszka <121969707+Mikokii@users.noreply.github.com>
2024-11-28 20:59:49 +0100

    Updated SortingMadnessApplication.java

--- src/main/java/pl/put/poznan/sorting/app/SortingMadnessApplication.java
+++ src/main/java/pl/put/poznan/sorting/app/SortingMadnessApplication.java
@@ -2,10 +2,6 @@ package pl.put.poznan.sorting.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.put.poznan.sorting.logic.BubbleSort;
-import pl.put.poznan.sorting.logic.QuickSort;
-
-import java.util.Arrays;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.sorting.rest"})
