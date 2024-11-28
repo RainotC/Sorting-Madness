e6f5a5c2f7410c3d134c03c8e18c503501cd7386
rainot <olga24@onet.pl>
2024-11-28 01:21:23 +0100

    Add merge sort

--- src/main/java/pl/put/poznan/sorting/app/SortingMadnessApplication.java
+++ src/main/java/pl/put/poznan/sorting/app/SortingMadnessApplication.java
@@ -3,6 +3,7 @@ package pl.put.poznan.sorting.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.put.poznan.sorting.logic.BubbleSort;
+import pl.put.poznan.sorting.logic.MergeSort;
 import pl.put.poznan.sorting.logic.QuickSort;
 
 import java.util.Arrays;
@@ -13,7 +14,7 @@ public class SortingMadnessApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SortingMadnessApplication.class, args);
-        testQuickSort();
+        testMergeSort();
     }
 
     public static void testBubbleSort() {
@@ -28,6 +29,14 @@ public class SortingMadnessApplication {
     public static void testQuickSort() {
 
         QuickSort sorter = new QuickSort();
+        int[] array = {1, 6, 4, 7, 2, 8, 205,  100, 25,2};
+        sorter.sort(array, 0, array.length-1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void testMergeSort() {
+
+        MergeSort sorter = new MergeSort();
         int[] array = {1, 6, 4, 7, 2, 8, 205,  100, 25,2};
         sorter.sort(array, 0, array.length-1);
         System.out.println(Arrays.toString(array));
