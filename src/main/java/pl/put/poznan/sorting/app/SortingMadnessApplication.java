780339ca2c7203edaa15697526809531663af0be
Maja Komorowska <majakom863@gmail.com>
2024-11-30 22:49:09 +0100

    Added SortingMadnessApplication and app package documentation

--- src/main/java/pl/put/poznan/sorting/app/SortingMadnessApplication.java
+++ src/main/java/pl/put/poznan/sorting/app/SortingMadnessApplication.java
@@ -3,10 +3,18 @@ package pl.put.poznan.sorting.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * The {@code SortingMadnessApplication} class serves as the entry point for the
+ * Sorting Madness Spring Boot application. It initializes the application context
+ * and starts the embedded web server.
+ */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.sorting.rest"})
 public class SortingMadnessApplication {
-
+    /**
+     * The main method that starts the Sorting Madness application.
+     *
+     * @param args command-line arguments passed during application startup
+     */
     public static void main(String[] args) {
         SpringApplication.run(SortingMadnessApplication.class, args);
     }
---
+++ src/main/java/pl/put/poznan/sorting/app/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The {@code app} package contains the entry point for the Sorting Madness application.
+ * It is responsible for initializing the Spring Boot application and starting the embedded web server.
+ *
+ */
+package pl.put.poznan.sorting.app;
