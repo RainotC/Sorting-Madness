828bf85a9911ba7340a1a7d18aaad15bd06382ab
rainot <olga24@onet.pl>
2024-12-07 12:49:46 +0100

    Delete import that wasn't used

--- src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
+++ src/main/java/pl/put/poznan/sorting/rest/SortingMadnessController.java
@@ -1,6 +1,5 @@
 package pl.put.poznan.sorting.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
